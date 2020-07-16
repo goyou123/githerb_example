1c51ba3dea9ffdf70f452019d9f8c69912c41f76
cheonga-code <chengalongo@gmail.com>
2020-07-16 17:31:41 +0900

    ex2Activiy 코드 수정

---
+++ app/src/main/java/com/example/githerb_example/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.githerb_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //나는 코드를 치고있다
+        //배고프다
+        //왜케 하루가 빨릴가냐
+        //
+
+    }
+}
