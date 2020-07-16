86631c3db9e9c01460824496440f8720409c1db2
goeunchan <goyouchan@naver.com>
2020-07-16 17:23:16 +0900

    새로운 액티비티2번쨰꺼

---
+++ app/src/main/java/com/example/githerb_example/ex2Activity.java
@@ -0,0 +1,18 @@
+package com.example.githerb_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ex2Activity extends AppCompatActivity {
+    TextView text1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex2);
+
+        text1 = (TextView)findViewById(R.id.text);
+        text1.setText("충돌가능?");
+    }
+}
