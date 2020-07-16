dababdfc2ae906ad2791a55b1a352d5c96e3d731
goeunchan <goyouchan@naver.com>
2020-07-16 16:19:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/githerb_example/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.githerb_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+        text.setText("깃허브 연동 예제입니다.");
+    }
+}
