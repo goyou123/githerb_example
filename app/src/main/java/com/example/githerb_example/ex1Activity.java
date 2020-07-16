12e7fbb861a7d951ce4ba41a45684772b0b4f334
goeunchan <goyouchan@naver.com>
2020-07-16 17:17:48 +0900

    새로운 액티비티 2

---
+++ app/src/main/java/com/example/githerb_example/ex1Activity.java
@@ -0,0 +1,17 @@
+package com.example.githerb_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ex1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex1);
+        //배고프다
+        //오ㅓ늘메뉴는
+
+    }
+}
