177d4c7b6cf2808f139946d88094d711c3be48bf
goeunchan <goyouchan@naver.com>
2020-07-18 15:35:17 +0900

    버터나이프 플러그인

--- app/src/main/java/com/example/githerb_example/MainActivity.java
+++ app/src/main/java/com/example/githerb_example/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.githerb_example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
     TextView text;
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button3)
+    Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        text = (TextView)findViewById(R.id.text);
-        text.setText("충돌실험시작 - 은찬");
+//        text = (TextView)findViewById(R.id.text);
+//        text.setText("충돌실험시작 - 은찬");
 
 
         //김청아 추가
         //김청아 추가 22
         //김청아 추가 33
+    }
+
+    @OnClick({R.id.button1, R.id.button2, R.id.button3})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+        }
     }
 }
